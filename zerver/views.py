f88f835a6f1fe70b5f392ce759dea3eddf620f06
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-06-04 17:02:11 +0530

    zerver: Implement LimitOffsetPagination in rest framework.

--- zerver/views.py
+++ zerver/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from .models import BankDetail, Bank
 from .serializers import BankSerializer, BankDetailSerializer
 from rest_framework.decorators import api_view
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import status
 from django.http import HttpResponse
 from django.db.models import Q
@@ -9,8 +10,11 @@ from django.db.models import Q
 
 @api_view(['GET'])
 def branch(request):
+    paginator = LimitOffsetPagination()
     branches = BankDetail.objects.filter(Q(ifsc__icontains=request.GET['q']) | Q(branch__icontains=request.GET['q']) | Q(address__icontains=request.GET['q']) | Q(city__icontains=request.GET['q']) | Q(district__icontains=request.GET['q']) | Q(state__icontains=request.GET['q'])).order_by('ifsc')
-    return Response({'branches': BankDetailSerializer(branches, many=True).data, 'count': branches.count()}, status=status.HTTP_200_OK)
+    result_page = paginator.paginate_queryset(branches, request)
+    serializer = BankDetailSerializer(result_page, many=True)
+    return Response({'branches': (serializer.data), 'count': branches.count()}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
