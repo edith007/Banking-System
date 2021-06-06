d7d31d8207b6ee4241c977a8f48e21e476982fd5
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-06-06 23:33:04 +0530

    static: Added Dropdown menu and complete frontend part.

--- static/src/App.js
+++ static/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
@@ -8,10 +10,32 @@ function App() {
   axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
   axios.defaults.xsrfCookieName = "csrftoken";
 
-  const [bankData] = useState([]);
+  const [value, setValue] = useState("Select Branch");
+  const [bankData, setBankData] = useState([]);
   const [limit] = useState(1);
 
   console.log(limit);
+
+  const handleSelect = (e) => {
+  console.log(e);
+  setValue(e);
+};
+
+const getBankData = async () => {
+const res = await axios.get(
+  `http://127.0.0.1:8000/api/branches/?q=${value}&limit=${limit}`
+);
+
+if (res.data.branches.length === 0) {
+  setBankData([]);
+} else {
+  setBankData(res.data.branches);
+}
+};
+useEffect(() => {
+    getBankData();
+  }, [value, limit]);
+
 
   const renderHeader = () => {
     let headerElement = [
@@ -60,9 +84,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Bank Detail</h1>
-      <div className="app_top">
-      </div>
+      <h1>Bank Branches</h1>
+        <div>
+          <DropdownButton
+            alignRight
+            title={value}
+            id="dropdown-menu"
+            onSelect={handleSelect}
+          >
+            <Dropdown.Item eventKey="Bangalore">Lucknow</Dropdown.Item>
+            <Dropdown.Item eventKey="Mumbai">Bangalore</Dropdown.Item>
+            <Dropdown.Item eventKey="Delhi">Delhi</Dropdown.Item>
+            <Dropdown.Item eventKey="Nagpur">RTGS</Dropdown.Item>
+            <Dropdown.Item eventKey="Bhopal">Bhopal</Dropdown.Item>
+          </DropdownButton>
+        </div>
       <div className="tracking-table">
         <table id="employee">
           <thead>
